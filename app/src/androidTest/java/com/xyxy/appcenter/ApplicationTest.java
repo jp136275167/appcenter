880575d0ed2616c26a5aa7b6255090dcba51b33c
JP <jp1102020018@sina.cn>
2015-10-25 13:11:08 +0800

    创建工程

---
+++ app/src/androidTest/java/com/xyxy/appcenter/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.xyxy.appcenter;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
